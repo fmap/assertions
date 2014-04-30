0dda167d4e09800846f45ab516e1bcd41512c244
vi <me@vikramverma.com>
2014-05-01 00:33:06 +0800

    Restructure to work with case-insensitive filesystems; use cabal to run tests.

--- test/Assert.hs
+++ test/Assert.hs
@@ -1,7 +1,13 @@
-import Test.Assert (runAssertions)
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main (main) where
+
+import Control.Applicative ((<$>))
+import Data.String.Interpolate (i)
 import System.Cmd (system)
 import System.Exit (ExitCode(..))
-import Control.Applicative ((<$>))
+import Test.Assert (runAssertions)
+import Paths_assert (getDataFileName)
 
 is0 :: ExitCode -> Bool
 is0 = (== ExitSuccess)
@@ -13,10 +19,10 @@ quietly :: String -> IO ExitCode
 quietly = system . (++"&>/dev/null")
 
 main :: IO ()
-main = do
-  greenTest <- is0 <$> quietly "runhaskell test/fixtures/Green.hs"
-  redTest   <- is1 <$> quietly "runhaskell test/fixtures/Red.hs"
-  mixedTest <- is1 <$> quietly "runhaskell test/fixtures/Mixed.hs"
+main = getDataFileName "test/fixtures" >>= \fixtures -> do
+  greenTest <- is0 <$> quietly [i|runhaskell #{fixtures}/Green.hs|]
+  redTest   <- is1 <$> quietly [i|runhaskell #{fixtures}/Red.hs|]
+  mixedTest <- is1 <$> quietly [i|runhaskell #{fixtures}/Mixed.hs|]
   runAssertions $
     [ ("When all tests in a suite pass, it should exit with 0.", greenTest)
     , ("When some but not all of the tests in a suite pass, it should exit with 1.", mixedTest)
